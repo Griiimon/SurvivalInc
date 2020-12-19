677f00cb7d96fd06d893cc8c337ce307081e04db
Griiimon <soerenpoeren2@gmail.com>
2020-12-19 12:31:52 +0100

    isn't effected by anything other than sanity tendency. All effects have been moved to SanityTendencyModifier. Now using SimpleStatRecord instead of SanityRecord.

--- src/main/java/enginecrafter77/survivalinc/stats/impl/SanityModifier.java
+++ src/main/java/enginecrafter77/survivalinc/stats/impl/SanityModifier.java
@@ -1,92 +1,102 @@
 package enginecrafter77.survivalinc.stats.impl;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.shader.ShaderGroup;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityCreature;
-import net.minecraft.entity.monster.*;
-import net.minecraft.entity.passive.EntityAnimal;
-import net.minecraft.entity.passive.EntityTameable;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.entity.living.AnimalTameEvent;
-import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
-import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
+import com.google.common.collect.Range;
 
 import enginecrafter77.survivalinc.SurvivalInc;
 import enginecrafter77.survivalinc.config.ModConfig;
-import enginecrafter77.survivalinc.net.StatSyncMessage;
-import enginecrafter77.survivalinc.stats.SimpleStatRecord;
-import enginecrafter77.survivalinc.stats.StatCapability;
+import enginecrafter77.survivalinc.debug.SanityDebugCommand;
 import enginecrafter77.survivalinc.stats.StatProvider;
 import enginecrafter77.survivalinc.stats.StatRecord;
 import enginecrafter77.survivalinc.stats.StatRegisterEvent;
+import enginecrafter77.survivalinc.stats.SimpleStatRecord;
+import enginecrafter77.survivalinc.stats.StatCapability;
 import enginecrafter77.survivalinc.stats.StatTracker;
+import enginecrafter77.survivalinc.stats.effect.DamageStatEffect;
 import enginecrafter77.survivalinc.stats.effect.EffectApplicator;
+import enginecrafter77.survivalinc.stats.effect.FunctionalCalculator;
 import enginecrafter77.survivalinc.stats.effect.FunctionalEffectFilter;
+import enginecrafter77.survivalinc.stats.effect.PotionStatEffect;
 import enginecrafter77.survivalinc.stats.effect.SideEffectFilter;
-import enginecrafter77.survivalinc.stats.effect.ValueStatEffect;
+import enginecrafter77.survivalinc.util.Util;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.shader.ShaderGroup;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class SanityModifier implements StatProvider<SanityRecord> {
-	private static final long serialVersionUID = 6707924203617912749L;
+/**
+ * The class that handles heat radiation and
+ * it's associated interactions with the
+ * player entity.
+ * @author Enginecrafter77
+ */
+public class SanityModifier implements StatProvider<SimpleStatRecord> {
+	// TODO how to generate??
+	private static final long serialVersionUID = 6269992840749029918L;
 	
 	public static final ResourceLocation distortshader = new ResourceLocation(SurvivalInc.MOD_ID, "shaders/distort.json");
 	public static final SoundEvent staticbuzz = new SoundEvent(new ResourceLocation(SurvivalInc.MOD_ID, "staticbuzz"));
+
+	
 	public static final SanityModifier instance = new SanityModifier();
 	
-	public final EffectApplicator<SanityRecord> effects;
-	public final Map<Item, Float> foodSanityMap;
+	public final EffectApplicator<SimpleStatRecord> effects;
+
 	
 	public SanityModifier()
 	{
-		this.effects = new EffectApplicator<SanityRecord>();
-		this.foodSanityMap = new HashMap<Item, Float>();
+		this.effects = new EffectApplicator<SimpleStatRecord>();
 	}
 	
 	public void init()
 	{
 		MinecraftForge.EVENT_BUS.register(SanityModifier.class);
-		if(ModConfig.WETNESS.enabled) this.effects.add(SanityModifier::whenWet).addFilter(FunctionalEffectFilter.byPlayer(EntityPlayer::isInWater).invert());
-		this.effects.add(new ValueStatEffect(ValueStatEffect.Operation.OFFSET, 0.004F)).addFilter(FunctionalEffectFilter.byPlayer(EntityPlayer::isPlayerSleeping));
-		this.effects.add(SanityModifier::whenInDark).addFilter(HydrationModifier.isOutsideOverworld.invert());
-		this.effects.add(SanityModifier::playStaticNoise).addFilter(SideEffectFilter.CLIENT);
-		this.effects.add(SanityModifier::whenNearEntities);
-		this.effects.add(SanityModifier::sleepDeprivation);
 		
-		// Compile food value list
-		for(String entry : ModConfig.SANITY.foodSanityMap)
-		{
-			int separator = entry.lastIndexOf(' ');
-			Item target = Item.getByNameOrId(entry.substring(0, separator));
-			Float value = Float.parseFloat(entry.substring(separator + 1));
-			this.foodSanityMap.put(target, value);
-		}
+		this.effects.add(SanityModifier::playStaticNoise).addFilter(SideEffectFilter.CLIENT);
+
+	}
+	
+	@SubscribeEvent
+	public static void registerStat(StatRegisterEvent event)
+	{
+		event.register(SanityModifier.instance);
 	}
 	
 	@Override
-	public void update(EntityPlayer target, StatRecord record)
+	public void update(EntityPlayer player, StatRecord record)
 	{
-		if(target.isCreative() || target.isSpectator()) return;
+		if(player.isCreative() || player.isSpectator()) return;
 		
-		SanityRecord sanity = (SanityRecord)record;
-		++sanity.ticksAwake;
-		this.effects.apply(sanity, target);
+		StatTracker tracker = player.getCapability(StatCapability.target, null);
+		
+		if(tracker == null)
+			return;
+			
+		SanityRecord tendency = tracker.getRecord(SanityTendencyModifier.instance);
+
+		SimpleStatRecord sanity= (SimpleStatRecord) record;
+		
+		sanity.addToValue((float)(tendency.getValue() * ModConfig.SANITY.tendencyImpact));
 		sanity.checkoutValueChange();
 	}
 
@@ -97,34 +107,23 @@ public class SanityModifier implements StatProvider<SanityRecord> {
 	}
 
 	@Override
-	public SanityRecord createNewRecord()
+	public SimpleStatRecord createNewRecord()
 	{
-		return new SanityRecord();
+		SimpleStatRecord record = new SimpleStatRecord();
+		record.setValueRange(Range.closed(0F, 100F));
+		record.setValue((float)ModConfig.SANITY.startValue);
+		return record;
 	}
 	
 	@Override
-	public Class<SanityRecord> getRecordClass()
+	public Class<SimpleStatRecord> getRecordClass()
 	{
-		return SanityRecord.class;
+		return SimpleStatRecord.class;
 	}
 	
-	@SubscribeEvent
-	public static void registerStat(StatRegisterEvent event)
+	public static void playStaticNoise(SimpleStatRecord record, EntityPlayer player)
 	{
-		event.register(SanityModifier.instance);
-	}
-	
-	public static void sleepDeprivation(SanityRecord record, EntityPlayer player)
-	{		
-		if(record.getTicksAwake() > ModConfig.SANITY.sleepDeprivationMin)
-		{
-			record.addToValue(-(float)ModConfig.SANITY.sleepDeprivationDebuff * (record.getTicksAwake() - ModConfig.SANITY.sleepDeprivationMin) / (ModConfig.SANITY.sleepDeprivationMax - ModConfig.SANITY.sleepDeprivationMin));
-		}
-	}
-	
-	public static void playStaticNoise(SanityRecord record, EntityPlayer player)
-	{
-		float threshold = (float)ModConfig.SANITY.hallucinationThreshold * SanityRecord.values.upperEndpoint();
+		float threshold = (float)ModConfig.SANITY.hallucinationThreshold * 100f;
 		if(player.world.getWorldTime() % 160 == 0)
 		{
 			/**
@@ -154,61 +153,29 @@ public class SanityModifier implements StatProvider<SanityRecord> {
 		}
 	}
 	
-	public static void whenInDark(SanityRecord record, EntityPlayer player)
-	{
-		BlockPos position = new BlockPos(player.getPositionVector().add(0D, player.getEyeHeight(), 0D));
-		int lightlevel = player.world.getLight(position);
-		
-		// If there is not enough light, steve/alex feels anxious
-		if(lightlevel < ModConfig.SANITY.comfortLightLevel)
-		{
-			float darknesslevel = (float)(ModConfig.SANITY.comfortLightLevel - lightlevel) / (float)ModConfig.SANITY.comfortLightLevel;
-			record.addToValue((float)ModConfig.SANITY.darkSpookFactorBase * -darknesslevel);
-		}
-	}
-	
-	public static void whenWet(SanityRecord record, EntityPlayer player)
-	{
-		float boundary = (float)ModConfig.SANITY.wetnessAnnoyanceThreshold;
-		StatTracker stats = player.getCapability(StatCapability.target, null);
-		SimpleStatRecord wetness = stats.getRecord(WetnessModifier.instance);		
-		if(wetness.getNormalizedValue() > boundary)
-		{
-			record.addToValue(((wetness.getNormalizedValue() - boundary) / (1F - boundary)) * -(float)ModConfig.SANITY.maxWetnessAnnoyance);
-		}
-	}
-	
-	public static void whenNearEntities(SanityRecord record, EntityPlayer player)
-	{
-		BlockPos origin = player.getPosition();
-		Vec3i offset = new Vec3i(3, 3, 3);
-		
-		AxisAlignedBB box = new AxisAlignedBB(origin.subtract(offset), origin.add(offset));
-		List<EntityCreature> entities = player.world.getEntitiesWithinAABB(EntityCreature.class, box);
-		
-		float value = record.getValue();
-		for(EntityCreature creature : entities)
-		{
-			if(creature instanceof EntityTameable)
-			{
-				EntityTameable pet = (EntityTameable)creature;
-				// 4x bonus for tamed creatures. Having pets has it's perks :D
-				float bonus = pet.isTamed() ? (float)ModConfig.SANITY.tamedMobMultiplier : 1;
-				value += ModConfig.SANITY.friendlyMobBonus * bonus;
-			}
-			else if(creature instanceof EntityAnimal)
-				value += ModConfig.SANITY.friendlyMobBonus;
-			else if(creature instanceof EntityMob)
-				value -= ModConfig.SANITY.hostileMobModifier;
-		}
-		record.setValue(value);
-	}
 	
 	@SubscribeEvent
 	@SideOnly(Side.CLIENT)
 	public static void onClientWorldTick(TickEvent.PlayerTickEvent event)
 	{
 		if(event.side.isServer()) return;
+		
+		if(SanityDebugCommand.enabled)
+		{
+			// Sends debug information to the player each second about absolute sanity and sanity tendency 
+			if(event.player.world.getWorldTime() % 20 == 0 && event.phase == Phase.START)
+			{
+				StatTracker tracker = event.player.getCapability(StatCapability.target, null);
+				SimpleStatRecord sanity = tracker.getRecord(SanityModifier.instance);
+				SimpleStatRecord sanityTendency = tracker.getRecord(SanityTendencyModifier.instance);
+	
+				float total= sanity.getValue();
+				float tend= sanityTendency.getValue();
+				
+				event.player.sendMessage(new TextComponentString("Total: "+total+", Tend: "+tend));
+			}
+		}
+		
 		
 		Minecraft client = Minecraft.getMinecraft();
 		ShaderGroup shader = client.entityRenderer.getShaderGroup();
@@ -225,54 +192,5 @@ public class SanityModifier implements StatProvider<SanityRecord> {
 		event.getRegistry().register(SanityModifier.staticbuzz);
 	}
 	
-	@SubscribeEvent
-	public static void onPlayerWakeUp(PlayerWakeUpEvent event)
-	{
-		EntityPlayer player = event.getEntityPlayer();
-		if(event.shouldSetSpawn()) // If the "lying in bed" was successful (the player actually fell asleep)
-		{
-			StatTracker stats = player.getCapability(StatCapability.target, null);
-			SanityRecord sanity = stats.getRecord(SanityModifier.instance);
-			sanity.addToValue(sanity.getValueRange().upperEndpoint() * (float)ModConfig.SANITY.sleepResoration);
-			sanity.resetSleep();
-			SurvivalInc.proxy.net.sendToAll(new StatSyncMessage(player));
-			player.getFoodStats().setFoodLevel(player.getFoodStats().getFoodLevel() - 8);
-		}
-	}
 	
-	@SubscribeEvent
-	public static void onConsumeItem(LivingEntityUseItemEvent.Finish event)
-	{		
-		Entity ent = event.getEntity();	
-		if(ent instanceof EntityPlayer)
-		{
-			try
-			{
-				// Try to get the modifier from the map (throws NPE when no such mapping exists)
-				float mod = SanityModifier.instance.foodSanityMap.get(event.getItem().getItem());
-				
-				// Modify the sanity value
-				EntityPlayer player = (EntityPlayer)ent;
-				StatTracker stats = player.getCapability(StatCapability.target, null);
-				SimpleStatRecord sanity = stats.getRecord(SanityModifier.instance);
-				sanity.addToValue(mod);
-			}
-			catch(NullPointerException exc)
-			{
-				// Food simply doesn't have any sanity mapping associated
-			}
-		}
-	}
-	
-	@SubscribeEvent
-	public static void onTame(AnimalTameEvent event)
-	{
-		Entity ent = event.getEntity();
-		if(ent instanceof EntityPlayer)
-		{
-			StatTracker stat = ent.getCapability(StatCapability.target, null);
-			SimpleStatRecord sanity = stat.getRecord(SanityModifier.instance);
-			sanity.addToValue((float)ModConfig.SANITY.animalTameBoost);
-		}
-	}
 }
