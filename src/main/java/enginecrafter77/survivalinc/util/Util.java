a37f8ad8098d8bfa444ac81be8cf1ab56a2d8051
Griiimon <soerenpoeren2@gmail.com>
2020-12-19 08:17:51 +0100

    Utility class with lerp function

---
+++ src/main/java/enginecrafter77/survivalinc/util/Util.java
@@ -0,0 +1,12 @@
+package enginecrafter77.survivalinc.util;
+
+public class Util {
+
+	public static double lerp(float a, float b, float f) 
+	{
+	    return ((a * (1.0 - f)) + (b * f));
+	}
+	
+	
+	
+}
