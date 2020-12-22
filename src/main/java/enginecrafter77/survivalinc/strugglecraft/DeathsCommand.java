14eb11407a3d8817967379e8670d599596d1c264
Griiimon <>
2020-12-22 22:44:32 +0100

    added commands for trait module

---
+++ src/main/java/enginecrafter77/survivalinc/strugglecraft/DeathsCommand.java
@@ -0,0 +1,29 @@
+package enginecrafter77.survivalinc.strugglecraft;
+
+import enginecrafter77.survivalinc.config.ModConfig;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+
+public class DeathsCommand extends CommandBase{
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		if(ModConfig.TRAITS.enabled)
+		{
+			sender.sendMessage(new TextComponentString("Deaths: "+TraitModule.instance.deathCtr));
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "deaths";
+	}
+
+	@Override
+	public String getUsage(ICommandSender arg0) {
+		return "/deaths";
+	}
+}
---
+++ src/main/java/enginecrafter77/survivalinc/strugglecraft/TraitsCommand.java
@@ -0,0 +1,32 @@
+package enginecrafter77.survivalinc.strugglecraft;
+
+import enginecrafter77.survivalinc.config.ModConfig;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+
+public class TraitsCommand extends CommandBase{
+
+
+	@Override
+	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+		if(ModConfig.TRAITS.enabled)
+		{
+			for(TraitModule.TraitListEntry entry : TraitModule.instance.listTraits)
+				sender.sendMessage(new TextComponentString(entry.trait.traitName+" Lvl "+(entry.tier+1)));
+			
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "traits";
+	}
+
+	@Override
+	public String getUsage(ICommandSender arg0) {
+		return "/traits";
+	}
+}
