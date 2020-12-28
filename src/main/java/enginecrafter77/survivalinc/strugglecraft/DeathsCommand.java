b0c2c5597f970dfbc51c23aad46df71d996349c7
Griiimon <>
2020-12-28 17:08:50 +0100

    deaths command now using scoreboard value

--- src/main/java/enginecrafter77/survivalinc/strugglecraft/DeathsCommand.java
+++ src/main/java/enginecrafter77/survivalinc/strugglecraft/DeathsCommand.java
@@ -4,6 +4,7 @@ import enginecrafter77.survivalinc.config.ModConfig;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 
@@ -11,10 +12,11 @@ public class DeathsCommand extends CommandBase{
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		if(ModConfig.TRAITS.enabled)
-		{
-			sender.sendMessage(new TextComponentString("Deaths: "+TraitModule.instance.deathCtr));
-		}
+//		if(ModConfig.TRAITS.enabled)
+//		{
+//			sender.sendMessage(new TextComponentString("Deaths: "+TraitModule.instance.getDeaths((EntityPlayer)sender.getCommandSenderEntity())));
+			sender.sendMessage(new TextComponentString("Deaths: "+DeathCounter.getDeaths((EntityPlayer)sender.getCommandSenderEntity())));
+//		}
 	}
 
 	@Override
