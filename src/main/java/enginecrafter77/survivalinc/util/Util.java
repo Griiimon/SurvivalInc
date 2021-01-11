12d760ea2a225b86b4bf490f163835b5c4eb0678
Griiimon <>
2021-01-11 01:14:01 +0100

    new functions

--- src/main/java/enginecrafter77/survivalinc/util/Util.java
+++ src/main/java/enginecrafter77/survivalinc/util/Util.java
@@ -76,6 +76,12 @@ public class Util {
 		return (int)Math.floor(Math.random()*(max-min)+min);
 	}
 
+	public static int rnd(World world, int min, int max)
+	{
+		return (int)Math.floor(world.rand.nextFloat()*(max-min)+min);
+	}
+
+	
 	public static float rnd(float min, float max) {
 		return (float)(Math.random()*(max-min)+min);
 	}
@@ -108,6 +114,15 @@ public class Util {
 		
 		return p1.distance(p2);
 	}
+
+	public static double distance(EntityPlayer player, BlockPos pos2)
+	{
+		Point3d p1= new Point3d(player.posX, player.posY, player.posZ);
+		Point3d p2= new Point3d(pos2.getX()+0.5, pos2.getY()+0.5, pos2.getZ()+0.5);
+		
+		return p1.distance(p2);
+	}
+
 	
 	public static boolean thisClientOnly(EntityPlayer player)
 	{
